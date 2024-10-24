aa731126fedecde051b7a0cf7557e81d236e234e
Jairo Arango <jaarango@grupo-exito.com>
2024-10-24 16:54:16 -0500

    fin primera parte, ordenar
    se ordena los archivos basicos y funcionales se procede con la implementacion de GTM y GA4

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
+const scripts = [
+    'js/jquery-3.6.0.min.js',
+    'js/bootstrap.min.js',
+    // 'js/appear.min.js',
+    // 'js/slick.min.js',
+    'js/jquery.magnific-popup.min.js',
+    // 'js/isotope.pkgd.min.js',
+    // 'js/wow.js',
+    'js/script.js',
+    'google-tag-managerfooter'
+];
+
 // Cargar el header de forma dinámica
 fetch('includes/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
     });
+// fetch('includes/main.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementsByTagName('main')[0].innerHTML = data;
+//         console.log("aRCAG",data)
+//     });
+
 
 // Cargar el footer de forma dinámica
 fetch('includes/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
+
+        // Cargar y ejecutar todos los scripts
+         loadScriptsInOrder(scripts);
     });
 
 //     fetch('js/script.js')
@@ -30,38 +52,36 @@ fetch('includes/footer.html')
 //     .catch(error => {
 //         console.error('Hubo un problema al cargar el archivo JS:', error);
 //     });
-    const scripts = [
 
-        'js/jquery-3.6.0.min.js',
-        'js/bootstrap.min.js',
-        'js/appear.min.js',
-        'js/slick.min.js',
-        'js/jquery.magnific-popup.min.js',
-        'js/isotope.pkgd.min.js',
-        'js/wow.js',
-        'js/script.js',
-    ];
-         // Cargar y ejecutar todos los scripts
-         scripts.forEach(script => {
-            loadScript(script);
-        });
-    function loadScript(url) {
-        return fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error al cargar el archivo JS: ${url}`);
-                }
-                return response.text();
-            })
-            .then(scriptContent => {
+function loadScript(url) {
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error al cargar el archivo JS: ${url}`);
+            }
+            return response.text();
+        })
+        .then(scriptContent => {
+            try {
                 // Creamos un nuevo elemento <script>
                 const scriptElement = document.createElement('script');
                 scriptElement.textContent = scriptContent;
-
                 // Insertamos el script en el <body> para que se ejecute
                 document.body.appendChild(scriptElement);
-            })
-            .catch(error => {
-                console.error('Error al cargar y ejecutar el archivo JS:', error);
-            });
+            } catch (error) {
+                console.error('ARGA error JS:', error);
+            }
+
+        })
+        .catch(error => {
+            console.error('Error al cargar y ejecutar el archivo JS:', error);
+        });
+}
+
+async function loadScriptsInOrder(scripts) {
+    // Usa un bucle for...of para esperar a que cada script se cargue antes de cargar el siguiente
+    for (const script of scripts) {
+        await loadScript(script);  // Espera que el script actual se cargue antes de pasar al siguiente
     }
+    console.log('Todos los scripts han sido cargados en orden.');
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+sessionStorage.removeItem("lastClickPage")
 // Encuentra todos los enlaces que tienen el atributo data-page
 const links = document.querySelectorAll('a[data-page]');
 
@@ -7,8 +8,9 @@ links.forEach(function (link) {
         e.preventDefault();  // Evita la acción por defecto del enlace
         // Obtén el valor del atributo data-page, que contiene el nombre del archivo HTML a cargar
         const page = this.getAttribute('data-page');
-        // Usa fetch para obtener el contenido de la página solicitada
-        fetch(page)
+        if (sessionStorage.getItem("lastClickPage") !== page)
+        {
+            fetch(page)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Error al cargar la página');
@@ -16,6 +18,8 @@ links.forEach(function (link) {
                 return response.text();
             })
             .then(data => {
+                console.log("argapage",page)
+                document.getElementById('main-section').classList.remove("bg-black", 'text-white')
                 if (document.getElementById('myVideo')) {
                     document.getElementById('myVideo').remove()
                 }
@@ -23,24 +27,62 @@ links.forEach(function (link) {
                 document.getElementById("dinamicContent").innerHTML = data;
                 document.getElementById("main-header").classList.add('main-header', 'header-three', 'text-white')
                 document.getElementById("menu-mobile").classList.add('collapsed')
-                document.getElementById("menu-mobile").classList.add('collapsed')
                 document.getElementById("navbarSupportedContent1").classList.add('collapsed')
                 document.getElementById("navbarSupportedContent1").classList.remove('show')
                 document.getElementById("menu-mobile").setAttribute("aria-expanded", "false")
                 handlePreloader();
+                getBackground();
 
             })
             .catch(error => {
-                console.error('Hubo un problema con la carga de la página:', error);
-                document.getElementById('content').innerHTML = '<p>Error al cargar la página.</p>';
+                document.getElementById("dinamicContent").innerHTML = '<h2>Estamos trabajando para mejorar tu experiencia <br> volveremos pronto...</h2>';
+
             });
+        }
+        sessionStorage.setItem("lastClickPage",page)
     });
 });
 
-// 30. Preloader
+// Preloader
 function handlePreloader() {
     if ($('.preloader').length) {
         $('.preloader').delay(200).fadeOut(500);
     }
 }
 handlePreloader();
+
+$(window).on('scroll', function () {
+    // Header Style and Scroll to Top
+    function headerStyle() {
+        if ($('.main-header').length) {
+            var windowpos = $(window).scrollTop();
+            var siteHeader = $('.main-header');
+            var scrollLink = $('.scroll-top');
+            if (windowpos >= 50) {
+                siteHeader.addClass('fixed-header');
+                scrollLink.fadeIn(300);
+            } else {
+                siteHeader.removeClass('fixed-header');
+                scrollLink.fadeOut(300);
+            }
+        }
+    }
+
+    headerStyle();
+
+});
+
+function getBackground() {
+    const random = Math.random();
+    if (random > 0.5) {
+        document.getElementById('logo').src = 'images/logos/logo-negro.png';
+        document.getElementById('logom').src = 'images/logos/logo-negro.png';
+        document.getElementById("main-div-header").style.backgroundColor  = '#4fc7e7';
+        document.getElementsByTagName('h2')[0].classList.add("coloriavazul")
+    } else {
+        document.getElementById('logo').src = 'images/logos/logo.png';
+        document.getElementById('logom').src = 'images/logos/logo.png';
+        document.getElementById("main-div-header").style.backgroundColor  = '';
+        document.getElementsByTagName('h2')[0].classList.add("coloriavazulOs")
+    }
+}
